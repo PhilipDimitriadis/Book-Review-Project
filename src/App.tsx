9755c680cd1c444a59eda4931fa9fe6e03bc9ef0
Filippos Dimitriadis <philipdimitriadis@gmail.com>
2025-07-13 20:18:21 +0300

    feat: layout test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import './App.css'
 import Home from "./pages/Home.tsx";
+import Layout from "./components/Layout.tsx";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
 
   return (
     <>
-      <Home/>
+        <Layout>
+            <Routes>
+                <Route path="/" element={<Home/>} />
+            </Routes>
+        </Layout>
     </>
   )
 }
 
-export default App
+export default App;
