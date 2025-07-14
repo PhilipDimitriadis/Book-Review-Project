4063af098ad9746c36f732334c22a48942e652e9
Philip Dimitriadis <philipdimitriadis@gmail.com>
2025-07-14 23:44:47 +0300

    feat: basic style and buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,28 @@
 import {Link} from "react-router";
 
 const Header = () => {
+
     return (
-        <>
-            <header>
-                <div>
-                    <nav>
-                        <Link to="/" className="bg-blacl text-white hover:undeline">Home</Link>
-                    </nav>
+        <header className="bg-gray-900 p-4">
+            <div className="container mx-auto flex items-center justify-between">
+
+                <div className="text-white text-xl font-bold">
+                    <Link to="/" className="">
+                        Book Review
+                    </Link>
                 </div>
-            </header>
-        </>
+
+                <div className="text-white text-m font-bold">
+                    <Link
+                        to="/reviews"
+                        className=""
+                    >
+                        My Reviews
+                    </Link>
+                </div>
+
+            </div>
+        </header>
     )
 }
 
