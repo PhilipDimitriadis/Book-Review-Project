bff84c907abdddbe07906c849d578f47843b6e8a
Philip Dimitriadis <philipdimitriadis@gmail.com>
2025-09-03 19:31:42 +0300

    feat: AuthProvider implemented

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,24 @@ import Layout from "./components/Layout.tsx";
 import {Route, Routes} from "react-router-dom";
 import BookDetailWrapper from "./pages/BookDetailWrapper.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
+import {AuthProvider} from "./context/AuthProvider.tsx";
+import MyReviewsPage from "./pages/MyReviewsPage.tsx";
 
 function App() {
 
   return (
     <>
-        <Layout>
-            <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/book/:bookKey" element={<BookDetailWrapper/>}/>
-                <Route path="login" element={<LoginPage/>} />
+        <AuthProvider>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/book/:bookKey" element={<BookDetailWrapper/>}/>
+                    <Route path="login" element={<LoginPage/>} />
+                    <Route path="/reviews" element={<MyReviewsPage/>} />
 
-            </Routes>
-        </Layout>
+                </Routes>
+            </Layout>
+        </AuthProvider>
     </>
   )
 }
